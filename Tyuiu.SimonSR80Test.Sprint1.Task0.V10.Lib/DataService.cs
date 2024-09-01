b45dcf7e465df944d482394bdc7935384e9a9879
Sergey <simon.serezha1@yandex.ru>
2024-09-01 20:28:36 +0500

    Добавьте файлы проекта.

---
+++ Tyuiu.SimonSR80Test.Sprint1.Task0.V10.Lib/DataService.cs
@@ -0,0 +1,11 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint1;
+namespace Tyuiu.SimonSR80Test.Sprint1.Task0.V10.Lib
+{
+	public class DataService : ISprint1Task1V10
+	{
+		public double Calculate(double x, double y)
+		{
+			return 2;
+		}
+	}
+}
