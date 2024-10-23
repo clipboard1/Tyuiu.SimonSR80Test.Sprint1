46275f5ec0e45757096d34cf921a26ebfb1f3845
Sergey <simon.serezha1@yandex.ru>
2024-10-23 12:56:01 +0500

    Lib test no app

---
+++ Tyuiu.SimonSR80Test.Sprint1.Task1.V10.Test/DataService.cs
@@ -0,0 +1,11 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint1;
+namespace Tyuiu.SimonSR80Test.Sprint1.Task0.V10.Lib
+{
+	public class DataService : ISprint1Task0V10
+	{
+		public double Calculate()
+		{
+			return 2;
+		}
+	}
+}
